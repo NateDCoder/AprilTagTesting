709a7d7cbd05e962e0e12ae992d0814848b9636c
NateDCoder <rubyandtwixie@gmail.com>
2025-02-24 11:43:26 -0500

    main wasnt being updated

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ scene.add(ambientLight);
 
 // Load model
 const loader = new GLTFLoader();
-loader.load('/AprilTagTesting/dist/field.glb', (gltf) => {
+loader.load('/AprilTagTesting/field.glb', (gltf) => {
   scene.add(gltf.scene);
   gltf.scene.scale.set(0.01, 0.01, 0.01);
   gltf.scene.traverse((child) => {
